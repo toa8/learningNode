69a70cad0b936e78de2ddbd6800ade5e20fdc4cd
toa8 <talhaorhunakdas@gmail.com>
2024-01-08 17:01:28 +0300

    creating and listen simple web server with http package

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
+// Package imports
 const fs = require("fs");
+const http = require("http");
 
-// Blocking, synchronous way
+// ------------------------------
+// -- FILE READ AND WRITE
 
-const textRead = fs.readFileSync("./text/someText.txt", "utf-8");
+// # Blocking, synchronous way
 
-const textOut = `This is what we know about avacados: ${textRead}. \n Created at ${Date.now()}`;
+// const textRead = fs.readFileSync("./text/someText.txt", "utf-8");
 
-fs.writeFileSync("./text/output.txt", textOut);
+// const textOut = `This is what we know about avacados: ${textRead}. \n Created at ${Date.now()}`;
 
-console.log(textRead);
-console.log("File written!");
+// fs.writeFileSync("./text/output.txt", textOut);
 
-// Non-blocking, asynchronous way
+// console.log(textRead);
+// console.log("File written!");
 
-fs.readFile("./text/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./text/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./text/someText.txt", "utf-8", (err, data3) => {
-      console.log(data3);
-      fs.writeFile("./text/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("Your final file written 🎉");
-      });
-    });
-  });
+// # Non-blocking, asynchronous way
+
+// fs.readFile("./text/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./text/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./text/someText.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile("./text/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your final file written 🎉");
+//       });
+//     });
+//   });
+// });
+
+// console.log("gonna work first");
+
+// -------------------------------
+// -- SERVER
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
 });
 
-console.log("gonna work first");
+const port = 8000;
+
+server.listen(port, () => {
+  console.log(`Listening the request on port ${port}`);
+});
