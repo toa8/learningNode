eb45aa643c71d5b50971bf8d161f3055275a62e0
toa8 <talhaorhunakdas@gmail.com>
2024-01-08 16:42:30 +0300

    this is how we read and write files in node with asyncronous way (callback functions)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+// Blocking, synchronous way
+
 const textRead = fs.readFileSync("./text/someText.txt", "utf-8");
 
 const textOut = `This is what we know about avacados: ${textRead}. \n Created at ${Date.now()}`;
@@ -8,3 +10,19 @@ fs.writeFileSync("./text/output.txt", textOut);
 
 console.log(textRead);
 console.log("File written!");
+
+// Non-blocking, asynchronous way
+
+fs.readFile("./text/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./text/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./text/someText.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile("./text/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your final file written 🎉");
+      });
+    });
+  });
+});
+
+console.log("gonna work first");
